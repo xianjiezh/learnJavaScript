4e6a97130c0ff8fa3eb95331d69c10db78aeb8ba
xianjiezh <904292766@qq.com>
2017-11-04 19:46:56 +0800

    add function fib

--- script.js
+++ script.js
@@ -11,5 +11,15 @@ function fac(n) {
 
 
 
-
 // 实现斐波那契数列
+function fib(n){
+    if(n == 1){
+        return 1
+    }else if(n == 2){
+        return 1
+    }else{
+        return fib(n-1)+fib(n-2)
+    }
+}
+//console.log(fib(7))
+
