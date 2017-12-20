9feb9f6feb6123b4b54103994aef665a31b591cd
xianjiezh <904292766@qq.com>
2017-12-20 20:46:06 +0800

    修复一个bug

--- 萧井陌的函数练习/main.js
+++ 萧井陌的函数练习/main.js
@@ -3,14 +3,14 @@
 // 定义我们的log 函数
 // 这样，我们就可以用log() 来代替console.log() 了
 // apply 的作用是强行把console作为一个对象传进去
-var log = function(){
-    return console.log.apply(console,arguments)
+var log = function () {
+    return console.log.apply(console, arguments)
 }
 
 // 作业1
 // 实现一个加法口诀表
 // 法1 用两个for 循环的方法完成
-function add1(){
+function add1() {
     for (let i = 1; i < 10; i++) {
         for (let j = 1; j < i + 1; j++) {
             document.write(i + '+' + j + '=' + (i + j) + ', ')
@@ -21,34 +21,34 @@ function add1(){
 // add1()
 
 // 法2用分解的方法，先构建出每一行的内容，在把每一行画上去
-function addLine(n){
+function addLine(n) {
     var s = ''
     for (let i = 0; i <= 9; i++) {
-        
-        s +=`${n} + ${i} = ${n + i} `
+
+        s += `${n} + ${i} = ${n + i} `
     }
-    
-    return s 
+
+    return s
 }
-function addTwo(){
+function addTwo() {
     var arr = []
     for (let n = 1; n <= 9; n++) {
         arr.push(addLine(n))
     }
-    return arr.join(','+'\n')
+    return arr.join(',' + '\n')
 }
 // console.log(addTwo())
 
 
 // 作业2
 // 随机返回0或1
-function random01(n){
+function random01(n) {
     var m = []
     for (let i = 0; i < n; i++) {
         var a = Math.floor(Math.random() + 0.5)
         m.push(a)
     }
-    
+
 
     return m
 }
@@ -61,7 +61,7 @@ function random01(n){
 //     [1, 0, 1],
 //     [0, 0, 0],
 // ]
-function randomSquere01(n){
+function randomSquere01(n) {
     var arr = []
     for (let i = 0; i < n; i++) {
         let arr2 = random01(n)
@@ -73,7 +73,7 @@ function randomSquere01(n){
 
 // 作业 4
 // 返回一个包含 0 ，9的随机array，长度为n
-function random09(n){
+function random09(n) {
     console.log('random09')
     let arr = random01(n)
     let arr2 = []
@@ -98,24 +98,27 @@ function random09(n){
 // [0, 1, 9, 2, 9]
 
 // 规则是, 0 会被设置为左右两边 9 的数量
-function markedLine(n){
+function markedLine(n) {
     let arr = random09(n)
     let arr2 = []
     for (let i = 0; i < arr.length; i++) {
         arr2[i] = arr[i]
     }
-    for (let i = 1; i < arr.length; i++) {
+    console.log(arr)
+    console.log(arr2)
+    for (let i = 0; i < arr.length; i++) {
         arr2[i] = arr[i]
-        if(arr2[i] === 0){
-            if(arr2[i-1] === 9){
-                arr2[i] ++
-                if(arr2[i+1] === 9){
-                    arr2[i] ++
-                }
+        if (arr2[i] === 0) {
+            if (arr2[i-1] === 9) {
+                arr2[i] += 1
+            }
+            if(arr2[i+1] === 9){
+                arr2[i] += 1            
             }
         }
-        
     }
+
+
     return arr2
 }
 log(markedLine(10))
